010423ec274143a5dbd059f96eb3740b23e75abe
HeleJ <hele.juhanson@gmail.com>
2021-12-26 17:06:28 +0000

    Install django-summernote

--- scandic/urls.py
+++ scandic/urls.py
@@ -19,9 +19,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('summernote/', include('django_summernote.urls')),
+
     path('admin/', admin.site.urls),
     path('meals/' , include('meals.urls' , namespace='meals')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Resturant AdminPanel"
+admin.site.site_title = "Resturant App Admin "
+admin.site.site_index_title = "Welcome To Resturant Admin Panel"
