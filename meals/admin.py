e926b4a9c43a1a5a34356ea45edf430e57dadcde
HeleJ <hele.juhanson@gmail.com>
2021-12-26 17:13:25 +0000

    Add class MealsAdmin to admin.py

--- meals/admin.py
+++ meals/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Meals
+from django_summernote.admin import SummernoteModelAdmin
+
+from .models import Meals ,
+
+
+class MealsAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+    list_display = ['name' ,'preperation_time', 'people', 'price']
+    search_fields = ['name', 'description' ]
+    list_filter = ('people')
 
 admin.site.register(Meals)
