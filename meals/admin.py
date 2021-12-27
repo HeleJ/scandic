344be7da46ee52fd998622cd068719c9b02db0c4
HeleJ <hele.juhanson@gmail.com>
2021-12-27 21:42:50 +0000

    Add contact app views.py

--- meals/admin.py
+++ meals/admin.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 
 from django_summernote.admin import SummernoteModelAdmin
 
-from .models import Meals 
+from .models import Meals
 
 
-class MealsAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+class MealsAdmin(SummernoteModelAdmin, admin.ModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
-    list_display = ['name' ,'preperation_time', 'people', 'price']
-    search_fields = ['name', 'description' ]
+    list_display = ['name', 'preperation_time', 'people', 'price']
+    search_fields = ['name', 'description']
     list_filter = ('people')
+
 
 admin.site.register(Meals)
--- scandic/urls.py
+++ scandic/urls.py
@@ -24,12 +24,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('meals/', include('meals.urls', namespace='meals')),
     path('reserve_table/', include('reservation.urls', namespace='reservation')),
+    path('contact/', include('contact.urls', namespace='contact')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-admin.site.site_header = "Resturant AdminPanel"
-admin.site.site_title = "Resturant App Admin "
-admin.site.site_index_title = "Welcome To Resturant Admin Panel"
+admin.site.site_header = "Restaurant AdminPanel"
+admin.site.site_title = "Restaurant App Admin "
+admin.site.site_index_title = "Welcome To Restaurant Admin Panel"
